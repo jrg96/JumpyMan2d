0924ae304c9c71a4ac70dcd94099c0ce82fc21c8
jrg96 <gomezlopez.jorge96@gmail.com>
2024-02-25 20:56:56 -0800

    Removing unnecessary initialization for player movement

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -25,11 +25,6 @@ public class Player : GenericSingleton<Player>
     // Start is called before the first frame update
     private void Start()
     {
-        /*
-         * Configurations for player
-         */
-        _playerMovement.MovementSpeed = 5f;
-        _playerMovement.JumpForce = 3f;
     }
 
     // Update is called once per frame
